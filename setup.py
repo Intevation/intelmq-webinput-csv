5f482a995eacff8d983f6f2e06ada31c039e3a65
Raimund Renkert <raimund.renkert@intevation.de>
2021-09-16 15:07:24 +0200

    Updated setup and apache example.

--- setup.py
+++ setup.py
@@ -6,16 +6,9 @@ import os
 
 from setuptools import find_packages, setup
 
-DATA = [
-    ('/opt/intelmq/etc/examples',
-     ['intelmq_webinput_csv/etc/webinput_csv.conf',
-      ],
-     ),
-]
-
 exec(open(os.path.join(os.path.dirname(__file__),
                        'intelmq_webinput_csv/version.py')).read())  # defines __version__
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as handle:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as handle:
     README = handle.read().replace('<docs/',
                                    '<https://github.com/certat/intelmq-webinput-csv/blob/master/docs/')
 
@@ -29,13 +22,6 @@ setup(
         'intelmq>=1.1.0',
         ],
     test_suite='intelmq_webinput_csv.tests',
-    packages=find_packages(),
-    package_data={'intelmq_webinput_csv': [
-        'etc/webinput.conf',
-        'static/',
-    ]
-    },
-    include_package_data=True,
     url='https://github.com/certat/intelmq_webinput_csv/',
     license='AGPLv3 and MIT and OFL-1.1',
     description='This is a Flask-based web interface allowing the user to '
@@ -63,7 +49,6 @@ setup(
         'Topic :: Security',
     ],
     keywords='incident handling cert csirt',
-    data_files=DATA,
     entry_points={
         'console_scripts': [
             'intelmq_webinput_csv=intelmq_webinput_csv.bin.backend:main',
