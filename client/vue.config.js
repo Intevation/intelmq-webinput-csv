5b719f39f62cffb35252341df0e6e43afa50a623
Raimund Renkert <raimund.renkert@intevation.de>
2021-09-27 16:55:44 +0200

    Removed 'webinput' from development proxy url.

--- client/vue.config.js
+++ client/vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     disableHostCheck: true,
     proxy: {
       "/api": {
-        target: "http://localhost:8002/webinput"
+        target: "http://localhost:8002"
       }
     }
   }
