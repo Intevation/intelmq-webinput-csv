6fbdfa0dc4d5cd8d5ec3108db4886132717598af
Sascha Wilde <wilde@sha-bang.de>
2021-09-30 11:17:14 +0200

    Simplified and updated setup.py
    Don't list intelmq as install-requirement, as this re-deploys intelmq binaries?

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Copyright (c) 2017-2018 nic.at GmbH <wagner@cert.at>
+# Copyright (c) 2021 Bundesamt für Sicherheit in der Informationstechnik
+# Authors:
+#   Sascha Wilde <wilde@intevation.de>
 # SPDX-License-Identifier: AGPL-3.0
 import os
 
@@ -8,50 +11,22 @@ from setuptools import find_packages, setup
 
 exec(open(os.path.join(os.path.dirname(__file__),
                        'intelmq_webinput_csv/version.py')).read())  # defines __version__
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as handle:
-    README = handle.read().replace('<docs/',
-                                   '<https://github.com/certat/intelmq-webinput-csv/blob/master/docs/')
-
 setup(
     name='intelmq_webinput_csv',
     version=__version__,
-    maintainer='Sebastian Wagner',
-    maintainer_email='wagner@cert.at',
+    author='Intevation GmbH',
+    author_email='info@intevation.de',
+    maintainer='Raimund Renkert',
+    maintainer_email='raimund.renkert@intevation.de',
+    packages=find_packages(),
     install_requires=[
-        'Flask',
-        'intelmq>=1.1.0',
+        'hug',
         ],
-    test_suite='intelmq_webinput_csv.tests',
-    url='https://github.com/certat/intelmq_webinput_csv/',
-    license='AGPLv3 and MIT and OFL-1.1',
-    description='This is a Flask-based web interface allowing the user to '
+    license='AGPLv3',
+    description='This is a simple web interface allowing the user to '
                 'insert data into intelmq\'s pipelines interactively with '
                 'preview from the parser.',
-    long_description=README,
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Framework :: Flask',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Telecommunications Industry',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Security',
-    ],
+
     keywords='incident handling cert csirt',
-    entry_points={
-        'console_scripts': [
-            'intelmq_webinput_csv=intelmq_webinput_csv.bin.backend:main',
-        ],
-    },
+    scripts = ['webinput-adduser']
 )
