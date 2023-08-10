42e5782f4103732f9af79e7cadf81078f435fb8e
Sebastian Wagner <swagner@intevation.de>
2023-08-10 16:02:04 +0200

    ci: pkg build: execute apt commands with sudo
    github containers are not running with user root as act does

--- .github/workflows/scripts/debian-package.sh
+++ .github/workflows/scripts/debian-package.sh
@@ -13,13 +13,13 @@ if [ "$ID" == "debian" ]; then
 elif [ "$ID" == "ubuntu" ]; then
     os_repo_name="xUbuntu"
 fi
-echo "deb http://download.opensuse.org/repositories/home:/sebix:/intelmq/${os_repo_name}_${VERSION_ID}/ /" > /etc/apt/sources.list.d/intelmq.list
-wget -O - https://download.opensuse.org/repositories/home:sebix:intelmq/${os_repo_name}_${VERSION_ID}/Release.key | apt-key add -
-echo "deb https://deb.nodesource.com/node_14.x ${VERSION_CODENAME} main" > /etc/apt/sources.list.d/nodesource.list
-wget -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarnpkg.list
-wget -O - https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-apt update
+echo "deb http://download.opensuse.org/repositories/home:/sebix:/intelmq/${os_repo_name}_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/intelmq.list
+wget -O - https://download.opensuse.org/repositories/home:sebix:intelmq/${os_repo_name}_${VERSION_ID}/Release.key | sudo apt-key add -
+echo "deb https://deb.nodesource.com/node_14.x ${VERSION_CODENAME} main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+wget -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarnpkg.list
+wget -O - https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+sudo apt update
 
 # install build dependencies
 DEBIAN_FRONTEND="noninteractive" sudo -E apt-get update -qq
