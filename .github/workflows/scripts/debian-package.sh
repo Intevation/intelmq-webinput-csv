3b308dfa5e8cc2c3768864c570e7d2213b0ab0cb
Sebastian Wagner <swagner@intevation.de>
2023-08-10 15:58:25 +0200

    ci, pkg: run package build, packge dependency fixes

--- .github/workflows/scripts/debian-package.sh
+++ .github/workflows/scripts/debian-package.sh
@@ -3,8 +3,23 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 #
 
-set -x
-set -e
+set -xeu -o pipefail
+
+# defines $ID, $VERSION_ID
+. /etc/os-release
+
+if [ "$ID" == "debian" ]; then
+    os_repo_name="Debian"
+elif [ "$ID" == "ubuntu" ]; then
+    os_repo_name="xUbuntu"
+fi
+echo "deb http://download.opensuse.org/repositories/home:/sebix:/intelmq/${os_repo_name}_${VERSION_ID}/ /" > /etc/apt/sources.list.d/intelmq.list
+wget -O - https://download.opensuse.org/repositories/home:sebix:intelmq/${os_repo_name}_${VERSION_ID}/Release.key | apt-key add -
+echo "deb https://deb.nodesource.com/node_14.x ${VERSION_CODENAME} main" > /etc/apt/sources.list.d/nodesource.list
+wget -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarnpkg.list
+wget -O - https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+apt update
 
 # install build dependencies
 DEBIAN_FRONTEND="noninteractive" sudo -E apt-get update -qq
